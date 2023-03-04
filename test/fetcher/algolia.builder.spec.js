632db6f590a7854a9d7372e760389ff72fba2d9c
Javier Galarza <jegj57@gmail.com>
2023-03-04 07:21:05 -0500

    chore: update tests

--- test/fetcher/algolia.builder.spec.js
+++ test/fetcher/algolia.builder.spec.js
@@ -1,28 +1,64 @@
 const tap = require('tap')
-const { buildBody } = require('../../lib/fetcher/algolia.builder')
+const { buildBodyRequest } = require('../../lib/fetcher/algolia.builder')
 
-tap.test('buildBody', (t) => {
-  t.plan(3)
+tap.test('buildBodyRequest', (t) => {
+  t.plan(7)
 
-  t.test('buildBody function must return an object always', (tt) => {
+  t.test('buildBodyRequest function must return an object always', (tt) => {
     tt.plan(1)
     const qry = 'gta'
-    const body = buildBody(qry)
-    tt.equal(typeof body === 'object', true)
+    const response = buildBodyRequest(qry)
+    tt.equal(typeof response === 'object', true)
   })
 
-  t.test('buildBody function must return an object with the property "method" and must be equal to POST', (tt) => {
+  t.test('buildBodyRequest function must return an object with the property "method" and must be equal to POST', (tt) => {
     tt.plan(2)
     const qry = 'gta'
-    const body = buildBody(qry)
-    tt.hasProp(body, 'method', 'does not has method property')
-    tt.equal(body.method, 'POST', 'method property is not equal')
+    const response = buildBodyRequest(qry)
+    tt.hasProp(response, 'method', 'does not has method property')
+    tt.equal(response.method, 'POST', 'method property is not equal')
   })
 
-  t.test('buildBody function must return an object with the property "body"', (tt) => {
+  t.test('buildBodyRequest function must return an object with the property "body" and must be an object always', (tt) => {
+    tt.plan(2)
+    const qry = 'gta'
+    const response = buildBodyRequest(qry)
+    tt.hasProp(response, 'body', 'does not has query body')
+    tt.equal(typeof response.body === 'object', true, 'body property is not an object')
+  })
+
+  t.test('body property must has the query property', (tt) => {
     tt.plan(1)
     const qry = 'gta'
-    const body = buildBody(qry)
-    tt.hasProp(body, 'body', 'does not has query body')
+    const response = buildBodyRequest(qry)
+    tt.hasProp(response.body, 'query', 'body does not has query property')
+  })
+
+  t.test('body property must has the attributesToHighlight property and must be an empty array', (tt) => {
+    tt.plan(3)
+    const qry = 'gta'
+    const response = buildBodyRequest(qry)
+    tt.hasProp(response.body, 'attributesToHighlight', 'body does not has attributesToHighlight property')
+    tt.ok(Array.isArray(response.body.attributesToHighlight))
+    tt.equal(response.body.attributesToHighlight.length, 0)
+  })
+
+  t.test('body property must has the attributesToSnippet property and must be an empty array', (tt) => {
+    tt.plan(3)
+    const qry = 'gta'
+    const response = buildBodyRequest(qry)
+    tt.hasProp(response.body, 'attributesToSnippet', 'body does not has attributesToSnippet property')
+    tt.ok(Array.isArray(response.body.attributesToSnippet))
+    tt.equal(response.body.attributesToSnippet.length, 0)
+  })
+
+  t.test('body property must has the facets property and must be an array with tags as the only value as default', (tt) => {
+    tt.plan(4)
+    const qry = 'gta'
+    const response = buildBodyRequest(qry)
+    tt.hasProp(response.body, 'facets', 'body does not has facets property')
+    tt.ok(Array.isArray(response.body.facets))
+    tt.equal(response.body.facets.length, 1)
+    tt.equal(response.body.facets[0], 'tags')
   })
 })
