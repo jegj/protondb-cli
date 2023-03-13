013f7a0333f27005d646e11467d5a4107b7ff5ed
Javier Galarza <jegj57@gmail.com>
2023-03-13 06:01:58 -0500

    chore: first approach for fetcher tests

--- test/fetcher/index.spec.js
+++ test/fetcher/index.spec.js
@@ -1,6 +1,22 @@
-const t = require('tap')
+const tap = require('tap')
 
-t.test('', (tt) => {
-  tt.plan(1)
-  tt.equal(null, null)
+const fetch = async (url) => {
+  throw new Error('unknown url: ' + url)
+}
+
+tap.test('algolia.fetcher(unhappy path)', (t) => {
+  t.plan(1)
+
+  t.test('fetcher must throw an error if there is a problem requesting to algolia API', async tt => {
+    tt.plan(1)
+    const fetcher = tt.mock('../../lib/fetcher/index.js', {
+      'node-fetch': fetch
+    })
+    try {
+      await fetcher.angoliaRequest()
+      tt.fail('error is expected')
+    } catch (error) {
+      tt.type(error, Error)
+    }
+  })
 })
