1b6910e26d7ea3bb214f4939bc31a52215635b16
Javier Galarza <jegj57@gmail.com>
2023-03-15 07:45:15 -0500

    chore: fix typo

--- test/fetcher/index.spec.js
+++ test/fetcher/index.spec.js
@@ -18,13 +18,13 @@ const mockFetchOk = async (url) => {
 tap.test('algolia.fetcher(unhappy path)', (t) => {
   t.plan(6)
 
-  t.test('angoliaRequest must throw an error if the query is not provided', async tt => {
+  t.test('algoliaFetcher must throw an error if the query is not provided', async tt => {
     tt.plan(2)
     const fetcher = tt.mock('../../lib/fetcher/index.js', {
       'node-fetch': mockFetchOk
     })
     try {
-      await fetcher.angoliaRequest()
+      await fetcher.algoliaFetcher()
       tt.fail('error is expected')
     } catch (error) {
       tt.type(error, Error)
@@ -32,13 +32,13 @@ tap.test('algolia.fetcher(unhappy path)', (t) => {
     }
   })
 
-  t.test('angoliaRequest must throw an error if the url is not provided', async tt => {
+  t.test('algoliaFetcher must throw an error if the url is not provided', async tt => {
     tt.plan(2)
     const fetcher = tt.mock('../../lib/fetcher/index.js', {
       'node-fetch': mockFetchOk
     })
     try {
-      await fetcher.angoliaRequest({ query: 'fifa' })
+      await fetcher.algoliaFetcher({ query: 'fifa' })
       tt.fail('error is expected')
     } catch (error) {
       tt.type(error, Error)
@@ -46,13 +46,13 @@ tap.test('algolia.fetcher(unhappy path)', (t) => {
     }
   })
 
-  t.test('angoliaRequest must throw an error if the algoliaApiKey is not provided', async tt => {
+  t.test('algoliaFetcher must throw an error if the algoliaApiKey is not provided', async tt => {
     tt.plan(2)
     const fetcher = tt.mock('../../lib/fetcher/index.js', {
       'node-fetch': mockFetchOk
     })
     try {
-      await fetcher.angoliaRequest({ query: 'fifa', url: 'angolia.api' })
+      await fetcher.algoliaFetcher({ query: 'fifa', url: 'angolia.api' })
       tt.fail('error is expected')
     } catch (error) {
       tt.type(error, Error)
@@ -60,13 +60,13 @@ tap.test('algolia.fetcher(unhappy path)', (t) => {
     }
   })
 
-  t.test('angoliaRequest must throw an error if the algoliaApplicationId is not provided', async tt => {
+  t.test('algoliaFetcher must throw an error if the algoliaApplicationId is not provided', async tt => {
     tt.plan(2)
     const fetcher = tt.mock('../../lib/fetcher/index.js', {
       'node-fetch': mockFetchOk
     })
     try {
-      await fetcher.angoliaRequest({ query: 'fifa', url: 'angolia.api', algoliaApiKey: 'x1x11212' })
+      await fetcher.algoliaFetcher({ query: 'fifa', url: 'angolia.api', algoliaApiKey: 'x1x11212' })
       tt.fail('error is expected')
     } catch (error) {
       tt.type(error, Error)
@@ -74,13 +74,13 @@ tap.test('algolia.fetcher(unhappy path)', (t) => {
     }
   })
 
-  t.test('angoliaRequest must throw an error if there is a problem requesting to algolia API', async tt => {
+  t.test('algoliaFetcher must throw an error if there is a problem requesting to algolia API', async tt => {
     tt.plan(1)
     const fetcher = tt.mock('../../lib/fetcher/index.js', {
       'node-fetch': mockFetchErr
     })
     try {
-      await fetcher.angoliaRequest({ query: 'fifa', url: 'angolia.api', algoliaApiKey: 'x1x11212', algoliaApplicationId: 'X2123ZAS123' })
+      await fetcher.algoliaFetcher({ query: 'fifa', url: 'angolia.api', algoliaApiKey: 'x1x11212', algoliaApplicationId: 'X2123ZAS123' })
       tt.fail('error is expected')
     } catch (error) {
       tt.type(error, Error)
@@ -93,7 +93,7 @@ tap.test('algolia.fetcher(unhappy path)', (t) => {
       'node-fetch': mockFetchOk
     })
     try {
-      const result = await fetcher.angoliaRequest({ query: 'fifa', url: 'angolia.api', algoliaApiKey: 'x1x11212', algoliaApplicationId: 'X2123ZAS123' })
+      const result = await fetcher.algoliaFetcher({ query: 'fifa', url: 'angolia.api', algoliaApiKey: 'x1x11212', algoliaApplicationId: 'X2123ZAS123' })
       tt.hasProp(result, 'hits', 'does not has hits property')
     } catch (error) {
       tt.fail('error is not expected')
