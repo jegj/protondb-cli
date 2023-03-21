1143ed8ea344115f21f0ed9f1e4745bf20fe0949
Javier Galarza <jegj57@gmail.com>
2023-03-21 07:12:04 -0500

    chore: update poc

--- poc/index.js
+++ poc/index.js
@@ -1,6 +1,7 @@
-const { algoliaFetcher } = require('../lib/fetcher')
+const { algoliaFetcher, protondbFetcher } = require('../lib/fetcher')
 
 const getGames = async () => {
+  console.log('## get games ##')
   const url = 'https://94he6yatei-dsn.algolia.net/1/indexes/steamdb/query'
   const algoliaApiKey = '9ba0e69fb2974316cdaec8f5f257088f'
   const algoliaApplicationId = '94HE6YATEI'
@@ -10,4 +11,18 @@ const getGames = async () => {
   console.log('===>', body)
 }
 
+const getGameInfo = async () => {
+  console.log('## get game info ##')
+  const url = 'https://www.protondb.com/api/v1/reports/summaries'
+  const objectId = '240'
+  const query = 'fifa'
+
+  const response = await protondbFetcher({ query, url, objectId })
+  console.log('===>', response)
+  const body = await response.json()
+  console.log('===>', body)
+}
+
 getGames()
+
+getGameInfo()
