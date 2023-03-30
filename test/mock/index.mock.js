e18d1f6c8dfc304e337beaea3c13720c3a8d4ffb
Javier Galarza <jegj57@gmail.com>
2023-03-30 07:09:50 -0500

    chore: add function for display games on blessed

--- test/mock/index.mock.js
+++ test/mock/index.mock.js
@@ -47,3 +47,232 @@ export const fetchProtondbMockedData = {
   total: 29,
   trendingTier: 'platinum'
 }
+
+export const mergedGameDataUncomplete = {
+  lastUpdated: 1653580973,
+  name: 'FIFA 22',
+  oslist: ['Windows', 'Steam Deck Playable'],
+  userScore: 79.24,
+  followers: 97900,
+  technologies: [],
+  releaseYear: 2021,
+  tags: [],
+  objectID: '1506830',
+  protondbNotFound: true
+}
+
+export const mergedGameDataComplete = {
+  lastUpdated: 1668605739,
+  name: 'Graze Counter GM',
+  oslist: ['Windows', 'Steam Deck Playable'],
+  userScore: 86,
+  followers: 1100,
+  technologies: ['Engine.GameMaker'],
+  releaseYear: 2023,
+  tags: [
+    'Action', 'Indie',
+    'Casual', 'Story Rich',
+    'Arcade', 'Shooter',
+    'Lore-Rich', '2D',
+    'Sci-fi', 'Pixel Graphics',
+    'Difficult', 'Anime',
+    'Singleplayer', "Shoot 'Em Up",
+    'Dark Fantasy', 'Cute',
+    'Mechs', 'Bullet Hell',
+    "1990's", 'Female Protagonist'
+  ],
+  objectID: '1486440',
+  bestReportedTier: 'platinum',
+  confidence: 'low',
+  score: 0.51,
+  tier: 'platinum',
+  total: 4,
+  trendingTier: 'platinum'
+}
+
+export const mergedGames = [
+  {
+    lastUpdated: 1666823474,
+    name: 'Counter-Strike',
+    oslist: ['Windows', 'macOS', 'Linux', 'Steam Deck Playable'],
+    userScore: 96.34,
+    followers: 197000,
+    technologies: [
+      'Engine.GoldSource',
+      'SDK.CEF',
+      'SDK.Miles_Sound_System',
+      'SDK.SDL'
+    ],
+    releaseYear: 2000,
+    tags: [
+      'Strategy', 'Action',
+      'Survival', 'FPS',
+      'Classic', 'Tactical',
+      'Shooter', 'PvP',
+      'First-Person', 'Multiplayer',
+      'Competitive', 'Old School',
+      'Military', 'Assassin',
+      'eSports', 'Score Attack',
+      'Team-Based', "1990's",
+      '1980s', 'Nostalgia'
+    ],
+    objectID: '10',
+    bestReportedTier: 'platinum',
+    confidence: 'good',
+    score: 0.45,
+    tier: 'gold',
+    total: 27,
+    trendingTier: 'gold'
+  },
+  {
+    lastUpdated: 1666823711,
+    name: 'Counter-Strike: Source',
+    oslist: ['Windows', 'macOS', 'Linux', 'Steam Deck Playable'],
+    userScore: 94.99,
+    followers: 438600,
+    technologies: [
+      'Engine.Source',
+      'SDK.Bink_Video',
+      'SDK.Miles_Sound_System',
+      'SDK.OpenVR',
+      'SDK.SDL'
+    ],
+    releaseYear: 2004,
+    tags: [
+      'Strategy', 'Action',
+      'Simulation', 'Survival',
+      'FPS', 'Moddable',
+      'War', 'Tactical',
+      'Shooter', 'PvP',
+      'Sandbox', 'First-Person',
+      'Multiplayer', 'Competitive',
+      'Difficult', 'Atmospheric',
+      'Military', 'Singleplayer',
+      'eSports', 'Team-Based'
+    ],
+    objectID: '240',
+    bestReportedTier: 'platinum',
+    confidence: 'good',
+    score: 0.57,
+    tier: 'gold',
+    total: 29,
+    trendingTier: 'platinum'
+  },
+  {
+    lastUpdated: 1567584599,
+    name: 'Counter-Strike: Condition Zero',
+    oslist: ['Windows', 'macOS', 'Linux', 'Steam Deck Playable'],
+    userScore: 89.27,
+    followers: 45500,
+    technologies: [
+      'Engine.GoldSource',
+      'SDK.CEF',
+      'SDK.Miles_Sound_System',
+      'SDK.SDL'
+    ],
+    releaseYear: 2004,
+    tags: [
+      'Strategy', 'Action',
+      'Adventure', 'Simulation',
+      'Survival', 'FPS',
+      'Classic', 'Open World',
+      'Tactical', 'Shooter',
+      'First-Person', 'Online Co-Op',
+      'Multiplayer', 'Competitive',
+      'Old School', 'Atmospheric',
+      'Military', 'Singleplayer',
+      'Dark', 'Team-Based'
+    ],
+    objectID: '80',
+    bestReportedTier: 'platinum',
+    confidence: 'moderate',
+    score: 0.53,
+    tier: 'platinum',
+    total: 11,
+    trendingTier: 'platinum'
+  },
+  {
+    lastUpdated: 1612588023,
+    name: 'Counter-Strike: Global Offensive',
+    oslist: ['Windows', 'macOS', 'Linux', 'Steam Deck Playable'],
+    userScore: 88.23,
+    followers: 3489400,
+    technologies: [
+      'Engine.Source',
+      'Engine.Source2',
+      'SDK.AMD_GPU_Services',
+      'SDK.Miles_Sound_System',
+      'SDK.NVIDIA_Nsight_Aftermath',
+      'SDK.Qt',
+      'SDK.SDL',
+      'SDK.Steam_Audio',
+      'SDK.Steam_Networking'
+    ],
+    releaseYear: 2012,
+    tags: [
+      'Strategy', 'Action',
+      'FPS', 'Moddable',
+      'War', 'Co-op',
+      'Tactical', 'Fast-Paced',
+      'Shooter', 'PvP',
+      'First-Person', 'Online Co-Op',
+      'Multiplayer', 'Competitive',
+      'Difficult', 'Military',
+      'Realistic', 'Trading',
+      'eSports', 'Team-Based'
+    ],
+    objectID: '730',
+    bestReportedTier: 'platinum',
+    confidence: 'strong',
+    score: 0.6,
+    tier: 'gold',
+    total: 568,
+    trendingTier: 'gold'
+  },
+  {
+    lastUpdated: 1640831140,
+    name: 'Over the Counter',
+    oslist: ['Windows', 'macOS', 'Linux'],
+    userScore: 84.98,
+    followers: 300,
+    technologies: ['Engine.Godot'],
+    releaseYear: 2022,
+    tags: [
+      'Strategy',
+      'Simulation',
+      '2D',
+      'Pixel Graphics',
+      'Singleplayer',
+      'Capitalism',
+      'Isometric',
+      'Resource Management',
+      'Profile Features Limited',
+      'Medical Sim'
+    ],
+    objectID: '1626460',
+    protondbNotFound: true
+  },
+  {
+    lastUpdated: 1666984670,
+    name: 'CounterAttack',
+    oslist: ['Windows', 'macOS', 'Linux'],
+    userScore: 83.13,
+    followers: 2400,
+    technologies: ['Engine.Unity', 'SDK.Photon'],
+    releaseYear: 2019,
+    tags: [
+      'Action', 'Adventure',
+      'Indie', 'Co-op',
+      'Space', 'Arcade',
+      'Shooter', 'Side Scroller',
+      'Local Co-Op', 'Online Co-Op',
+      'Multiplayer', 'Sci-fi',
+      'Retro', 'Difficult',
+      'Singleplayer', "Shoot 'Em Up",
+      'Co-op Campaign', '4 Player Local',
+      'Bullet Hell', 'Local Multiplayer'
+    ],
+    objectID: '451600',
+    protondbNotFound: true
+  }
+]
---
+++ test/presenter/formatter.spec.js
@@ -0,0 +1,59 @@
+import tap from 'tap'
+import { formatGame, format } from '../../lib/presenter/formatter.js'
+import { mergedGameDataComplete, mergedGameDataUncomplete, mergedGames } from '../mock/index.mock.js'
+
+tap.test('formatGame', async (t) => {
+  t.plan(6)
+
+  t.test('formatGame must return an array always', (tt) => {
+    tt.plan(1)
+    const result = formatGame(mergedGameDataComplete)
+    tt.ok(Array.isArray(result))
+  })
+
+  t.test('first item must be the game\'s name', (tt) => {
+    tt.plan(1)
+    const result = formatGame(mergedGameDataComplete)
+    tt.equal(result[0], mergedGameDataComplete.name)
+  })
+
+  t.test('2th item must be the game\'s tier if there are result from protondb API', (tt) => {
+    tt.plan(1)
+    const result = formatGame(mergedGameDataComplete)
+    tt.equal(result[1], mergedGameDataComplete.tier)
+  })
+
+  t.test('3th item must be the game\'s confidence if there are result from protondb API', (tt) => {
+    tt.plan(1)
+    const result = formatGame(mergedGameDataComplete)
+    tt.equal(result[2], mergedGameDataComplete.confidence)
+  })
+
+  t.test('2th item must be equal to "pending" if the has not result from protondb API', (tt) => {
+    tt.plan(1)
+    const result = formatGame(mergedGameDataUncomplete)
+    tt.equal(result[1], 'pending')
+  })
+
+  t.test('3th item must be equal to "pending" if the has not result from protondb API', (tt) => {
+    tt.plan(1)
+    const result = formatGame(mergedGameDataUncomplete)
+    tt.equal(result[2], 'pending')
+  })
+})
+
+tap.test('format', async (t) => {
+  t.plan(1)
+
+  t.test('format must return an array of arrays and each array child must have a length of 3', (tt) => {
+    const asserts = 2 + mergedGames.length * 2
+    tt.plan(asserts)
+    const result = format(mergedGames)
+    tt.ok(Array.isArray(result))
+    tt.equal(result.length, mergedGames.length)
+    result.forEach((gameFormated) => {
+      tt.ok(Array.isArray(gameFormated))
+      tt.equal(gameFormated.length, 3)
+    })
+  })
+})
