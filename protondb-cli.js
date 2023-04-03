bd419f6bfe52cab99ba2fe774ac609add1d5f5bb
Javier Galarza <jegj57@gmail.com>
2023-04-03 07:14:05 -0500

    chore: add config store

--- protondb-cli.js
+++ protondb-cli.js
@@ -3,11 +3,9 @@
 import yargs from 'yargs/yargs'
 import { hideBin } from 'yargs/helpers'
 import start from './lib/process/index.js'
-import { readFileSync } from 'fs'
 import { isValidUrl } from './lib/utils.js'
 import chalk from 'chalk'
-
-const OPTS = JSON.parse(readFileSync('./default.json'))
+import config from './lib/config/index.js'
 
 const protondbCLI = yargs(hideBin(process.argv))
   .scriptName('protondb-cli')
@@ -28,33 +26,33 @@ const protondbCLI = yargs(hideBin(process.argv))
       alias: 'h',
       type: 'number',
       description: 'Limit the number of result on the search',
-      default: OPTS.DEFAULT_PROTONDB_CLI_HITS
+      default: config.get('DEFAULT_PROTONDB_CLI_HITS')
     }).option('concurrency', {
       alias: 'c',
       type: 'number',
       description: 'Limit the concurrency for the search',
-      default: OPTS.DEFAULT_PROTONDB_CLI_CONCURRENCY
+      default: config.get('DEFAULT_PROTONDB_CLI_CONCURRENCY')
     }).option('algolia_query_url', {
       alias: 'aqu',
       type: 'string',
-      description: 'Algolia main URL for the search. Obtain from https://www.protondb.com/',
-      default: OPTS.DEFAULT_ALGOLIA_QUERY_URL,
+      description: 'Algolia main URL for the search.',
+      default: config.get('DEFAULT_ALGOLIA_QUERY_URL'),
       coerce: isValidUrl
     }).option('algolia_api_key', {
       alias: 'aak',
       type: 'string',
-      description: 'Algolia API key. Obtain from https://www.protondb.com/',
-      default: OPTS.DEFAULT_X_ALGOLIA_API_KEY
+      description: 'Algolia API key.',
+      default: config.get('DEFAULT_X_ALGOLIA_API_KEY')
     }).option('algolia_application_id', {
       alias: 'aai',
       type: 'string',
-      description: 'Algolia application id. Obtain from https://www.protondb.com/',
-      default: OPTS.DEFAULT_X_ALGOLIA_APPLICATION_ID
+      description: 'Algolia application id.',
+      default: config.get('DEFAULT_X_ALGOLIA_APPLICATION_ID')
     }).option('protondb_url', {
       alias: 'pu',
       type: 'string',
-      description: 'Protondb URL for search game information. Obtain from https://www.protondb.com/',
-      default: OPTS.DEFAULT_PROTONDB_URL,
+      description: 'Protondb URL for search game information.',
+      default: config.get('DEFAULT_PROTONDB_URL'),
       coerce: isValidUrl
     }).example([
       ['$0 gta --concurrency 5 --hits 15', 'Search the last 15 like gta using an conccurency of 5']
