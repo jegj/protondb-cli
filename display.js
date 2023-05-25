d625a783b2098bfcdb8f6ac3ed4cc0232ec1a3e9
Javier Galarza <jegj57@gmail.com>
2023-05-24 19:25:09 -0500

    chore: adjust display script

--- display.js
+++ display.js
@@ -1,32 +1,14 @@
 /* eslint-disable new-cap */
 import blessed from 'blessed'
 import contrib from 'blessed-contrib'
+import terminalImage from 'terminal-image'
+
+const image = await terminalImage.file('header.jpg', { width: '25%', height: '25%' })
 
 const screen = blessed.screen()
-const grid = new contrib.grid({ rows: 5, cols: 1, screen })
+const grid = new contrib.grid({ rows: 1, cols: 2, screen })
 
-const line = grid.set(0, 0, 1, 1, contrib.line,
-  {
-    style:
-       {
-         line: 'yellow',
-         text: 'green',
-         baseline: 'black'
-       },
-    xLabelPadding: 3,
-    xPadding: 5,
-    label: 'Stocks'
-  })
-
-// eslint-disable-next-line no-unused-vars
-const map = grid.set(1, 0, 1, 1, contrib.map, { label: 'Servers Location' })
-
-const lineData = {
-  x: ['t1', 't2', 't3', 't4'],
-  y: [5, 1, 7, 5]
-}
-
-line.setData([lineData])
+grid.set(0, 0, 1, 1, blessed.text, { label: 'Image', content: image })
 
 screen.key(['escape', 'q', 'C-c'], function (ch, key) {
   return process.exit(0)
--- poc.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import terminalImage from 'terminal-image'
-
-console.log(await terminalImage.file('header.jpg', { width: '25%', height: '25%' }))
