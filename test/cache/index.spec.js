e31e785ce082a63b823fc9ef874605566808a1d8
Javier Galarza <jegj57@gmail.com>
2023-11-24 04:54:49 -0500

    chore: use node native test runner and assertion

--- test/cache/index.spec.js
+++ test/cache/index.spec.js
@@ -1,19 +1,21 @@
-import tap from 'tap'
 import { createCache } from '../../lib/cache/index.js'
 import os from 'node:os'
 import { join } from 'node:path'
 import { rmdir } from 'node:fs/promises'
+import { test, describe, afterEach } from 'node:test'
+import assert from 'node:assert'
 
 const testCacheFolder = join(os.homedir(), '.config', 'protondbcli_test')
 
-tap.after(async () => {
-  await rmdir(testCacheFolder)
-})
+describe('cache', async () => {
+  afterEach(async () => {
+    await rmdir(testCacheFolder)
+  })
 
-tap.test('createCache method must return always a lowdb cache object which contains the data property with all the protondb-cli keys', async (tt) => {
-  tt.plan(3)
-  const cache = await createCache(testCacheFolder)
-  tt.hasOwnProp(cache, 'data')
-  tt.hasOwnProp(cache.data, 'etags')
-  tt.hasOwnProp(cache.data, 'games')
+  test('createCache method must return always a lowdb cache object which contains the data property with all the protondb-cli keys', async () => {
+    const cache = await createCache(testCacheFolder)
+    assert(Object.prototype.hasOwnProperty.call(cache, 'data'), 'cache does not have the data property')
+    assert(Object.prototype.hasOwnProperty.call(cache.data, 'etags'), 'cache data does not have the etags property')
+    assert(Object.prototype.hasOwnProperty.call(cache.data, 'games'), 'cache data does not have the games property')
+  })
 })
